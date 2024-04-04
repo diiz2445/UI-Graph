3cc19c3e0cd7a7aa10b803a563b9307740946ebd
Tara Routray <ax90790@gmail.com>
2024-04-04 19:06:25 +0500

    UI commit 1

---
+++ UI Graph/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UI_Graph.Core
+{
+    class ObservableObject : INotifyPropertyChanged//класс для реализации интерфейса, чтобы обрабатывать изменения(по сути нажатия кнопок)
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
