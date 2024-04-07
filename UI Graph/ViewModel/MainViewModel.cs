b0f3ab98864c9e6c0ea52c1ff60b3ee68ee8b97d
Tara Routray <ax90790@gmail.com>
2024-04-07 22:03:52 +0500

    add Models of view

---
+++ UI Graph/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI_Graph.Core;
+
+namespace UI_Graph.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SmejnViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public SmejnViewModel SmejnVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); } 
+            
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            SmejnVM = new SmejnViewModel();
+
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView =HomeVM; });
+            SmejnViewCommand = new RelayCommand(o => { CurrentView =SmejnVM; });
+        }
+
+    }
+}
